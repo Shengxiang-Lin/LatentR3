1dded8705e2485ae1ebb14f7ce818b5f21823d26
Shengxiang-Lin <2792674719@qq.com>
2025-08-11 15:08:56 +0800

    update

--- src/utils/calc.py
+++ src/utils/calc.py
@@ -7,8 +7,8 @@ import math
 import json
 import pandas as pd
 import numpy as np
-    
 from tqdm import tqdm
+
 def gao(path, item_path):
     print(path)
     print(item_path)
@@ -17,7 +17,6 @@ def gao(path, item_path):
     if item_path.endswith(".txt"):
         item_path = item_path[:-4]
     CC=0
-
     f = open(f"{item_path}.txt", 'r')
     items = f.readlines()
     item_names = [_.split('\t')[0].strip("\"").strip(" ").strip('\n').strip('\"') for _ in items]
@@ -31,7 +30,6 @@ def gao(path, item_path):
     # print(item_dict) 
     ALLNDCG = np.zeros(5) # 1 3 5 10 20
     ALLHR = np.zeros(5)
-
     result_dict = dict()
     topk_list = [1, 3, 5, 10, 20]
     for p in path:
@@ -43,11 +41,8 @@ def gao(path, item_path):
         import json
         test_data = json.load(f)
         f.close()
-        
         text = [ [_.strip(" \n").strip("\"").strip(" ") for _ in sample["predict"]] for sample in test_data]
-        
         for index, sample in tqdm(enumerate(text)):
-
                 if type(test_data[index]['output']) == list:
                     target_item = test_data[index]['output'][0].strip("\"").strip(" ")
                 else:
@@ -58,7 +53,6 @@ def gao(path, item_path):
                     # for _ in item_dict[target_item]:
                         # if rank[i][0] == _:
                             # minID = i
-                    
                     if sample[i] not in item_dict:
                         CC += 1
                         # print(sample[i], index)
